37607ff22a21e8ca4a469e7941cc39a006962acf
Ulya06 <top-32@mail.ru>
2026-07-16 19:27:22 +0200

    pud

--- conftest.py
+++ conftest.py
@@ -3,13 +3,18 @@ from selenium import webdriver
 from pages.cart_page import CartPage
 from pages.category_page import CategoryPage
 from pages.product_page import ProductPage
+from selenium.webdriver.chrome.options import Options
+import allure
 
 
 @pytest.fixture()
 def driver():
-    chrome_driver = webdriver.Chrome()
-    chrome_driver.maximize_window()
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument("window-size = 1920, 1080")
+    chrome_driver = webdriver.Chrome(options=options)
     yield chrome_driver
+    allure.attach(chrome_driver.get_screenshot_as_png(), name = "screenshot", attachment_type=AttachmentType.PNG)
     chrome_driver.quit()
 
 @pytest.fixture()

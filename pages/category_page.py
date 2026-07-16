9c17c51de1419c26895c2cdf4777dfa317970877
Ulya06 <top-32@mail.ru>
2026-07-16 19:55:15 +0200

    gg

--- pages/category_page.py
+++ pages/category_page.py
@@ -1,9 +1,11 @@
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 from pages.locators.category_locators import CategoryLocators
 
 
 class CategoryPage(BasePage):
+
     def open_page(self):
         self.open(self.base_url + 'shop/category/desks-1')
 
@@ -14,65 +16,90 @@ class CategoryPage(BasePage):
         assert name in self.driver.page_source
 
     def open_first_product(self):
-        self.wait.until(
-            EC.element_to_be_clickable(CategoryLocators.PRODUCT_NAME)
-        ).click()
+        self.click(CategoryLocators.PRODUCT_NAME)
 
     def switch_to_list_view(self):
-        self.wait.until(
-            EC.element_to_be_clickable(CategoryLocators.LIST_VIEW_BTN)
-        ).click()
+
+        button = self.wait.until(
+            EC.presence_of_element_located(
+                CategoryLocators.LIST_VIEW_BTN
+            )
+        )
+
+        self.driver.execute_script(
+            "arguments[0].click();",
+            button
+        )
 
         self.wait.until(
-            EC.presence_of_element_located(
-                CategoryLocators.LIST_VIEW_CONTAINER
-            )
+            lambda driver: "o_wsale_layout_list" in driver.page_source
         )
 
     def select_steel_filter(self):
         checkbox = self.wait.until(
-            EC.element_to_be_clickable(CategoryLocators.STEEL_CHECKBOX)
+            EC.presence_of_element_located(
+                CategoryLocators.STEEL_CHECKBOX
+            )
         )
 
-        self.driver.execute_script("arguments[0].click();",checkbox)
+        self.driver.execute_script(
+            "arguments[0].click();",
+            checkbox
+        )
 
-        self.wait.until(EC.url_contains("attrib=1-1"))
+        self.wait.until(
+            EC.url_contains("attrib=1-1")
+        )
 
     def click_cart_icon(self):
         icon = self.wait.until(
-            EC.element_to_be_clickable(CategoryLocators.ADD_TO_CART_ICON)
+            EC.element_to_be_clickable(
+                CategoryLocators.ADD_TO_CART_ICON
+            )
         )
 
-        self.driver.execute_script("arguments[0].click();",icon)
+        self.driver.execute_script(
+            "arguments[0].click();",
+            icon
+        )
 
     def proceed_to_checkout(self):
         btn = self.wait.until(
-            EC.element_to_be_clickable(CategoryLocators.PROCEED_CHECKOUT_BTN)
+            EC.element_to_be_clickable(
+                CategoryLocators.PROCEED_CHECKOUT_BTN
+            )
         )
         btn.click()
 
     def should_be_on_cart_page(self):
-        self.wait.until(EC.url_contains("/shop/cart"))
+        self.wait.until(
+            EC.url_contains("/shop/cart")
+        )
 
     def verify_product_in_cart(self, full_name, description):
-        title = self.get_text(CategoryLocators.CART_PRODUCT_TITLE)
-        desc = self.get_text(CategoryLocators.CART_PRODUCT_DESCRIPTION)
+        title = self.get_text(
+            CategoryLocators.CART_PRODUCT_TITLE
+        )
+
+        desc = self.get_text(
+            CategoryLocators.CART_PRODUCT_DESCRIPTION
+        )
 
         assert full_name in title
         assert description in desc
 
     def change_currency_to_eur(self):
-        self.wait.until(
-            EC.element_to_be_clickable(CategoryLocators.PRICELIST_BTN)
-        ).click()
+        self.click(CategoryLocators.PRICELIST_BTN)
 
-        eur = self.wait.until(
-            EC.element_to_be_clickable(CategoryLocators.EUR_PRICELIST)
+        element = self.wait.until(
+            EC.element_to_be_clickable(
+                CategoryLocators.EUR_PRICELIST
+            )
         )
 
         self.driver.execute_script(
             "arguments[0].click();",
-            eur
+            element
         )
 
     def should_all_prices_be_in_eur(self):

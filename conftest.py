36937c75149a28a47b04787fea54a9416587df9e
Ulya06 <top-32@mail.ru>
2026-07-21 20:39:31 +0200

    up

--- conftest.py
+++ conftest.py
@@ -11,7 +11,7 @@ from allure_commons.types import AttachmentType
 @pytest.fixture()
 def driver():
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
